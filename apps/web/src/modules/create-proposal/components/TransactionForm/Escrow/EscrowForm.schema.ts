6ce9bfb52332dad116c6797af2a26cd2d073644c
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-10-04 11:37:40 +0530

    save: progress escrow form

--- apps/web/src/modules/create-proposal/components/TransactionForm/Escrow/EscrowForm.schema.ts
+++ apps/web/src/modules/create-proposal/components/TransactionForm/Escrow/EscrowForm.schema.ts
@@ -4,13 +4,26 @@ import type { AddressType } from 'src/typings'
 import { addressValidationSchema } from 'src/utils/yup'
 
 export interface EscrowFormValues {
-  recipientAddress?: string | AddressType
-  amount?: number
+  clientAddress: string | AddressType
+  recipientAddress: string | AddressType
+  safetyValveDate: Date
+  milestones: Array<{ title: string; description: string; amount: number }>
+  startDate: Date
+  endDate: Date
+  arbitrationProvider: string | AddressType
+  arbitrationCourt: number
 }
 
 const EscrowFormSchema = yup.object({
+  clientAddress: addressValidationSchema,
   recipientAddress: addressValidationSchema,
-  amount: yup.number().min(1, 'Must be at least 1 token').required(),
+  amount: yup.number().min(0.5, 'Must be at least 0.5 eth').required(),
+  safetyValveDate: yup.date().required(),
+  milestones: yup.array().of(yup.object().required()),
+  endDate: yup.date().required(),
+  startDate: yup.date().required(),
+  arbitrationProvider: addressValidationSchema,
+  arbitrationCourt: yup.number().required(),
 })
 
 export default EscrowFormSchema
--- apps/web/src/modules/create-proposal/components/TransactionForm/Escrow/EscrowForm.tsx
+++ apps/web/src/modules/create-proposal/components/TransactionForm/Escrow/EscrowForm.tsx
@@ -1,9 +1,11 @@
-import { Box, Button, Flex } from '@zoralabs/zord'
+import { Box, Button, Flex, Input as Input2 } from '@zoralabs/zord'
 import { Form, Formik } from 'formik'
 import type { FormikHelpers } from 'formik'
 import { useCallback } from 'react'
 import type { FC } from 'react'
 
+import TextArea from 'src/components/Fields/TextArea'
+import Accordion from 'src/components/Home/accordian'
 import { Icon } from 'src/components/Icon'
 import Input from 'src/components/Input/Input'
 
@@ -14,10 +16,67 @@ export interface EscrowFormProps {
   disabled?: boolean
 }
 
+const MilestoneForm: FC<any> = ({ title, description, amount, removeMilestone }) => {
+  return (
+    <Box>
+      <Input
+        name={'title'}
+        type={'text'}
+        placeholder={'Milestone Title'}
+        autoComplete={'off'}
+        // error={
+        //   touched.recipientAddress && errors.recipientAddress
+        //     ? errors.recipientAddress
+        //     : undefined
+        // }
+      />
+      <TextArea
+        id="description"
+        value=""
+        placeholder={'Milestone Description'}
+        inputLabel={''}
+        onChange={() => {}}
+        onBlur={() => {}}
+      />
+      <Input
+        name={'amount'}
+        type={'text'}
+        placeholder={'amount in eth'}
+        autoComplete={'off'}
+        // error={
+        //   touched.recipientAddress && errors.recipientAddress
+        //     ? errors.recipientAddress
+        //     : undefined
+        // }
+      />
+      <Button variant="outline" mt={'x5'} width={'auto'} onClick={removeMilestone}>
+        Remove
+      </Button>
+    </Box>
+  )
+}
+
 const EscrowForm: FC<EscrowFormProps> = ({ onSubmit, disabled }) => {
   const initialValues: EscrowFormValues = {
+    clientAddress: '',
     recipientAddress: '',
-    amount: 0,
+    milestones: [
+      {
+        title: 'Milestone 1',
+        description: 'About Milestone 1',
+        amount: 0,
+      },
+      {
+        title: 'Milestone 2',
+        description: 'About Milestone 2',
+        amount: 0,
+      },
+    ],
+    safetyValveDate: new Date(),
+    startDate: new Date(),
+    endDate: new Date(),
+    arbitrationProvider: '',
+    arbitrationCourt: 0,
   }
 
   const handleSubmit = useCallback(
@@ -26,6 +85,27 @@ const EscrowForm: FC<EscrowFormProps> = ({ onSubmit, disabled }) => {
     },
     [onSubmit]
   )
+
+  const milestones = initialValues.milestones.map((milestone, index) => ({
+    title: 'Milestone ' + (index + 1),
+    description: (
+      <MilestoneForm
+        key={index}
+        title={milestone.title}
+        description={milestone.description}
+        amount={milestone.amount}
+        removeMilestone={() => removeMilestone(index)}
+      />
+    ),
+  }))
+
+  const addMilestone = () => {
+    // add another milestone
+  }
+
+  const removeMilestone = (index: number) => {
+    // remove milestone
+  }
 
   return (
     <Box w={'100%'}>
@@ -44,7 +124,8 @@ const EscrowForm: FC<EscrowFormProps> = ({ onSubmit, disabled }) => {
             disabled={isValidating || disabled}
             style={{ outline: 0, border: 0, padding: 0, margin: 0 }}
           >
-            <Flex as={Form} direction={'column'}>
+            <Flex as={Form} direction={'column'} gap={'x5'}>
+              {/* Service Provider */}
               <Input
                 name={'recipientAddress'}
                 label={'Recipient Wallet Address/ENS'}
@@ -68,20 +149,37 @@ const EscrowForm: FC<EscrowFormProps> = ({ onSubmit, disabled }) => {
                     : undefined
                 }
               />
-
+              {/* Milestone Releasers or DAO */}
+              <Input
+                name={'clientAddress'}
+                label={'Client Wallet Address/ENS'}
+                type={'text'}
+                placeholder={'0x...'}
+                autoComplete={'off'}
+                secondaryLabel={
+                  <Icon
+                    id={'checkInCircle'}
+                    fill={'positive'}
+                    style={{
+                      opacity:
+                        typeof errors.clientAddress === 'undefined' && dirty ? 1 : 0,
+                      transition: '0.1s opacity',
+                    }}
+                  />
+                }
+                error={
+                  touched.clientAddress && errors.clientAddress
+                    ? errors.clientAddress
+                    : undefined
+                }
+              />
               <Box mt={'x5'}>
-                <Input
-                  name={'amount'}
-                  label={'Amount'}
-                  secondaryLabel={'ETH'}
-                  autoComplete={'off'}
-                  type={'number'}
-                  placeholder={0}
-                  min={0}
-                  error={touched.amount && errors.amount ? errors.amount : undefined}
-                />
+                {/* Milestones */}
+                <Accordion items={milestones} />
+                <Button variant="outline" mt={'x2'} width={'auto'} onClick={addMilestone}>
+                  Add Another Milestone
+                </Button>
               </Box>
-
               <Button
                 mt={'x9'}
                 variant={'outline'}
