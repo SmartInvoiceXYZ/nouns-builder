7a9a0860d21a4497bcbbcb5ac7e3b9d3238d88d0
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-10-17 20:34:09 +0530

    update ipfs metadata format

--- apps/web/src/modules/create-proposal/components/TransactionForm/Escrow/Escrow.tsx
+++ apps/web/src/modules/create-proposal/components/TransactionForm/Escrow/Escrow.tsx
@@ -1,9 +1,15 @@
 import { Stack } from '@zoralabs/zord'
 import { uploadFile } from 'ipfs-service'
+import { useParams, usePathname } from 'next/navigation'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
+import useSWR from 'swr'
 import { encodeFunctionData } from 'viem'
 import { useChainId } from 'wagmi'
 
+import SWR_KEYS from 'src/constants/swrKeys'
+import { ProposalsResponse } from 'src/data/subgraph/requests/proposalsQuery'
+import { getProposals } from 'src/data/subgraph/requests/proposalsQuery'
 import { TransactionType } from 'src/modules/create-proposal/constants'
 import { useProposalStore } from 'src/modules/create-proposal/stores'
 
@@ -22,17 +28,35 @@ export const Escrow: React.FC = () => {
   const [ipfsUploadError, setIpfsUploadError] = useState<Error | null>(null)
   const [ipfsCID, setIpfsCID] = useState<string>('')
 
+  const { query, isReady, basePath } = useRouter()
+
   const chainId = useChainId()
+
   const addTransaction = useProposalStore((state) => state.addTransaction)
   const removeTransactions = useProposalStore((state) => state.removeAllTransactions)
 
+  const { data } = useSWR<ProposalsResponse>(
+    isReady ? [SWR_KEYS.PROPOSALS, chainId, query.token, '0'] : null,
+    (_, chainId, token, page) => getProposals(chainId, token, 1, Number(0))
+  )
+
+  const lastProposalId = data?.proposals?.[0]?.proposalNumber ?? 0
+
   const handleEscrowTransaction = async (values: EscrowFormValues) => {
+    // Allow only single escrow transaction
     removeTransactions()
+
     const ipfsDataToUpload = {
+      title: 'Proposal #' + (lastProposalId + 1),
+      description: window?.location.href.replace(
+        '/proposal/create',
+        'vote/' + lastProposalId + 1
+      ),
       endDate: new Date(
         values.milestones[values.milestones.length - 1].deliveryDate
       ).getTime(),
-      milestones: values.milestones.map((x) => ({
+      milestones: values.milestones.map((x, index) => ({
+        id: 'milestone-00' + index,
         title: x.title,
         description: x.description,
         deliveryDate: new Date(x.deliveryDate).getTime(),
@@ -40,16 +64,21 @@ export const Escrow: React.FC = () => {
           ? {
               documents: [
                 {
+                  id: 'doc-001',
                   type: 'ipfs',
                   src: x.mediaUrl,
+                  mimeType: x.mediaType,
                   createdAt: new Date().getTime(),
                 },
               ],
             }
           : {}),
       })),
+      resolverType: 'kleros',
       totalAmount: values.milestones.reduce((acc, x) => acc + x.amount, 0),
       klerosCourt: 1,
+      createdAt: Date.now(),
+      startDate: Date.now() + 7 * 86400 * 1000, // set start date 7 days from submission
       arbitrationProvider: KLEROS_ARBITRATION_PROVIDER,
     }
 
