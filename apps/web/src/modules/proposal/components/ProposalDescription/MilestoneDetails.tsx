f203d9fe3e221e9e860f3321ad2c479ec82d12f3
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-10-23 15:02:30 +0530

    refactor Milestone details component

--- apps/web/src/modules/proposal/components/ProposalDescription/MilestoneDetails.tsx
+++ apps/web/src/modules/proposal/components/ProposalDescription/MilestoneDetails.tsx
@@ -13,6 +13,7 @@ import {
 import { useContractRead } from 'wagmi'
 
 import Accordion from 'src/components/Home/accordian'
+import { Icon } from 'src/components/Icon'
 import { OptionalLink } from 'src/components/OptionalLink'
 import SWR_KEYS from 'src/constants/swrKeys'
 import { TransactionType } from 'src/modules/create-proposal'
@@ -25,15 +26,19 @@ import { AddressType } from 'src/typings'
 
 import { DecodedTransaction } from './ProposalDescription'
 
-const releaseFunctionAbi = [
+interface MilestoneDetailsProps {
+  decodedTxnData: DecodedTransaction
+  executionTransactionHash?: string
+}
+
+interface Document {
+  type: string
+  src: string
+}
+
+const RELEASE_FUNCTION_ABI = [
   {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_milestone',
-        type: 'uint256',
-      },
-    ],
+    inputs: [{ internalType: 'uint256', name: '_milestone', type: 'uint256' }],
     name: 'release',
     outputs: [],
     stateMutability: 'nonpayable',
@@ -41,14 +46,21 @@ const releaseFunctionAbi = [
   },
 ]
 
-export const MilestoneDetails = ({
+// Constants
+const INVOICE_ADDRESS = '0x39f74e876f4c5c8a8e16ad2f543a3e89c3f7d784' // TODO: Get dynamically
+const SAFE_APP_URL =
+  'https://app.safe.global/share/safe-app?appUrl=https://app.smartinvoice.xyz/invoices'
+
+export const MilestoneDetails: React.FC<MilestoneDetailsProps> = ({
   decodedTxnData,
   executionTransactionHash,
-}: {
-  decodedTxnData: DecodedTransaction
-  executionTransactionHash?: string
 }) => {
-  // Decode Calldata to get ipfs CID
+  const router = useRouter()
+  const { chain: invoiceChain } = useChainStore()
+  const { addresses } = useDaoStore()
+  const { removeAllTransactions, addTransaction } = useProposalStore()
+
+  // Decode transaction data
   const decodedAbiData = decodeAbiParameters(
     parseAbiParameters([
       'address client',
@@ -62,43 +74,32 @@ export const MilestoneDetails = ({
     decodedTxnData?._escrowData?.value as Hex
   )
 
-  const router = useRouter()
-
-  const removeTransactions = useProposalStore((state) => state.removeAllTransactions)
-  const { chain: invoiceChain } = useChainStore()
-  const { addresses } = useDaoStore()
-  const addTransaction = useProposalStore((state) => state.addTransaction)
-
-  const invoiceAddress = `0x39f74e876f4c5c8a8e16ad2f543a3e89c3f7d784` // TODO: Get this dynamically from execution txn hash
   const invoiceCid = convertByte32ToIpfsCidV0((decodedAbiData as never)?.[4])
+  const clientAddress = (decodedAbiData as never)?.[0]
 
+  // Fetch invoice data from IPFS
   const { data: invoiceData } = useSWR(
     invoiceCid ? [SWR_KEYS.IPFS, invoiceCid] : null,
-    () => axios.get(`https://ipfs.io/ipfs/${invoiceCid}`).then((x) => x.data),
-    {
-      revalidateOnFocus: false,
-    }
+    async () => {
+      try {
+        const response = await axios.get(`https://ipfs.io/ipfs/${invoiceCid}`)
+        return response.data
+      } catch (error) {
+        console.error('Failed to fetch invoice data:', error)
+        return null
+      }
+    },
+    { revalidateOnFocus: false }
   )
 
-  const milestoneAmount = decodedTxnData['_milestoneAmounts']['value']
-    .split(',')
-    .map((x) => formatEther(BigInt(x)))
-
-  const milestones: IpfsMilestone[] = invoiceData?.milestones
-
-  const { data: numOfMilestonesReleased, isSuccess } = useContractRead({
-    address: invoiceAddress,
+  // Get number of released milestones
+  const { data: numOfMilestonesReleased } = useContractRead({
+    address: INVOICE_ADDRESS,
     abi: [
       {
         inputs: [],
         name: 'released',
-        outputs: [
-          {
-            internalType: 'uint256',
-            name: '',
-            type: 'uint256',
-          },
-        ],
+        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
         stateMutability: 'view',
         type: 'function',
       },
@@ -107,101 +108,117 @@ export const MilestoneDetails = ({
     chainId: invoiceChain.id,
   })
 
+  const milestoneAmount = decodedTxnData['_milestoneAmounts']['value']
+    .split(',')
+    .map((x: string) => formatEther(BigInt(x)))
+
   const handleReleaseMilestone = async (index: number) => {
-    console.log({ addresses })
+    const isClientGoverner = isAddressEqual(
+      clientAddress as AddressType,
+      addresses.governor as AddressType
+    )
 
-    // todo: check for multiSig being equal to client address on decoded invoice data
-    if (
-      isAddressEqual(addresses.multiSig as AddressType, addresses.governor as AddressType)
-    ) {
-      // clear up existing txns
-      removeTransactions()
-
-      const releaseMilestone = {
-        target: invoiceAddress as AddressType,
-        functionSignature: 'release(_milestone)',
-        calldata: encodeFunctionData({
-          abi: releaseFunctionAbi,
-          functionName: 'release',
-          args: [index],
-        }),
-        value: '',
-      }
-
-      const releaseEscrowTxnData = {
-        type: TransactionType.RELEASE_ESCROW_MILESTONE,
-        summary: `Release Milestone #${index + 1} for ${invoiceData?.title}`,
-        transactions: [releaseMilestone],
-      }
-
-      addTransaction(releaseEscrowTxnData)
-
-      // send user to review stage
-      setTimeout(() => {
-        router.push({
-          pathname: `/dao/[network]/[token]/proposal/review`,
-          query: {
-            network: router.query?.network,
-            token: router.query?.token,
-          },
-        })
-      }, 1000)
-    } else {
-      router.replace(
-        'https://app.safe.global/share/safe-app?appUrl=https://app.smartinvoice.xyz/invoices'
-      )
+    if (!isClientGoverner) {
+      router.replace(SAFE_APP_URL)
+      return
     }
+
+    removeAllTransactions()
+
+    const releaseMilestone = {
+      target: INVOICE_ADDRESS as AddressType,
+      functionSignature: 'release(_milestone)',
+      calldata: encodeFunctionData({
+        abi: RELEASE_FUNCTION_ABI,
+        functionName: 'release',
+        args: [index],
+      }),
+      value: '',
+    }
+
+    const releaseEscrowTxnData = {
+      type: TransactionType.RELEASE_ESCROW_MILESTONE,
+      summary: `Release Milestone #${index + 1} for ${invoiceData?.title}`,
+      transactions: [releaseMilestone],
+    }
+
+    addTransaction(releaseEscrowTxnData)
+
+    // Navigate to review page with release txn loaded
+    router.push({
+      pathname: `/dao/[network]/[token]/proposal/review`,
+      query: {
+        network: router.query?.network,
+        token: router.query?.token,
+      },
+    })
   }
 
-  const milestonesDetails = milestones?.map((milestone, index) => {
-    return {
-      title: (
-        <Text>
-          {index + 1}. {milestone.title}
-        </Text>
-      ),
-      description: (
-        <Stack gap={'x5'}>
-          <Stack direction={'row'} align={'center'} justify={'space-between'}>
-            <Text variant="label-xs" color="tertiary">
-              {'Amount: ' + milestoneAmount[index] + ' ETH'}
-            </Text>
-            <Text variant="label-xs" color="tertiary">
-              {'Due by: ' + new Date(milestone?.endDate as number).toLocaleDateString()}
-            </Text>
-          </Stack>
-          <Text>{milestone.description || 'No Description'}</Text>
-          <Stack>
-            {milestone.documents?.map((doc, index) => {
-              if (doc.src) {
-                return (
-                  <OptionalLink
-                    enabled={true}
-                    href={doc?.src.replace('ipfs://', 'https://ipfs.io/ipfs/')}
-                  >
-                    {doc.type === 'ipfs'
-                      ? doc?.src.replace('ipfs://', 'https://ipfs.io/ipfs/')
-                      : doc.src}
-                  </OptionalLink>
-                )
-              }
-            })}
-          </Stack>
-
-          {executionTransactionHash &&
-          Number(numOfMilestonesReleased?.toString()) === index ? (
-            <Button onClick={() => handleReleaseMilestone(index)}>
-              Release Milestone
-            </Button>
-          ) : (
-            <Button variant="secondary" disabled>
-              Release Milestone
-            </Button>
-          )}
-        </Stack>
-      ),
+  const renderMilestoneButton = (index: number, isReleased: boolean, isNext: boolean) => {
+    if (isReleased) {
+      return (
+        <Button variant="secondary" disabled>
+          <Icon id="checkInCircle" />
+          Milestone Released
+        </Button>
+      )
     }
-  })
 
-  return <Accordion items={milestonesDetails} />
+    return (
+      <Button
+        variant={isNext ? 'primary' : 'secondary'}
+        disabled={!isNext}
+        onClick={() => isNext && handleReleaseMilestone(index)}
+      >
+        Release Milestone
+      </Button>
+    )
+  }
+
+  const renderDocumentLink = (doc: Partial<Document>) => {
+    if (!doc.src) return null
+
+    const href =
+      doc.type === 'ipfs' ? doc.src.replace('ipfs://', 'https://ipfs.io/ipfs/') : doc.src
+
+    return (
+      <OptionalLink key={doc.src} enabled={true} href={href}>
+        {href}
+      </OptionalLink>
+    )
+  }
+
+  const milestonesDetails = invoiceData?.milestones?.map(
+    (milestone: IpfsMilestone, index: number) => {
+      const releasedCount = Number(numOfMilestonesReleased?.toString() || 0)
+      const isReleased = releasedCount - 1 >= index
+      const isNext = releasedCount === index
+
+      return {
+        title: <Text>{`${index + 1}. ${milestone.title}`}</Text>,
+        description: (
+          <Stack gap="x5">
+            <Stack direction="row" align="center" justify="space-between">
+              <Text variant="label-xs" color="tertiary">
+                {`Amount: ${milestoneAmount[index]} ETH`}
+              </Text>
+              <Text variant="label-xs" color="tertiary">
+                {`Due by: ${new Date(milestone?.endDate as number).toLocaleDateString()}`}
+              </Text>
+            </Stack>
+
+            <Text>{milestone.description || 'No Description'}</Text>
+
+            <Stack>
+              {milestone.documents?.map((doc, index) => renderDocumentLink(doc))}
+            </Stack>
+
+            {executionTransactionHash && renderMilestoneButton(index, isReleased, isNext)}
+          </Stack>
+        ),
+      }
+    }
+  )
+
+  return <Accordion items={milestonesDetails || []} />
 }
